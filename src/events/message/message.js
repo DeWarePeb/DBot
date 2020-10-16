0d3bac4cd48ea8a61bc96cf8e2c57f5e5d009704
DeWarePeb <68034923+DeWarePeb@users.noreply.github.com>
2020-10-16 21:17:15 +0200

    v0.1

---
+++ src/events/message/message.js
@@ -0,0 +1,14 @@
+const discord = require('discord.js');
+const config = require('../../config/config.json');
+
+module.exports = (client, message) => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(config.prefix)) return;
+  let cmdName = message.content.substring(message.content.indexOf(config.prefix) + 1).split(new RegExp(/\s+/)).shift();
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  if (client.commands.get(cmdName))
+    client.commands.get(cmdName)(client, message, args);
+  else
+    message.channel.send("No command is named that way.");
+  console.log("Command does not exist.");
+};
